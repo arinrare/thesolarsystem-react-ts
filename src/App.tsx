33edc843dae5c10a2ca2866382d4dd1366861a4c
MIKE-PC\micha <michaelbaggott@hotmail.com>
2024-04-08 13:58:02 +1000

    Initial Commit. First plugin conversion to React module Accordion Slider done.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ImageAccordion from './components/imageAccordion';
+
+function App() {
+  return (
+    <ImageAccordion />
+  );
+}
+
+export default App;
