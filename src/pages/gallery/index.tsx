716301db3cb00ebfd2c856b08c4eb54becae14af
MIKE-PC\micha <michael.baggott@conceptv.com.au>
2024-04-10 17:09:24 +1000

    Added nav links on all pages. Yet to implement the "selected" feature.

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './index.css';
+import Navmenu from '../../components/navmenu'
 
 const Gallery = () => {
     return (
         <div>
             <h1>Gallery</h1>
+            <Navmenu />
         </div>
     );
 }
