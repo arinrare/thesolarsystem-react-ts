c664154aeedd7d9bc630d5cb6c1d41574c567c4e
mbaggott <5018715+mbaggott@users.noreply.github.com>
2024-04-12 11:49:26 +1000

    Moved images that couyld be moved from public to within src folder. Added into blurb. Removed unused imports from navmeny. Added background image for App.

--- src/components/navmenu/index.tsx
+++ src/components/navmenu/index.tsx
@@ -1,6 +1,10 @@
-import React, {useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import './index.css';
+const homeImage = require('../../images/navmenu/home_resized.png');
+const distanceImage = require('../../images/navmenu/distance_resized.png');
+const orbitImage = require('../../images/navmenu/orbit_resized.png');
+const galleryImage = require('../../images/navmenu/gallery_resized.png');
 
 const Navmenu = () => {
     function handleClick(ID: string) {
@@ -10,10 +14,10 @@ const Navmenu = () => {
     return (
         <>
             <div id="navmenu">
-                <Link onClick={() => handleClick("0")} className= "navlink inline" to="/"><img className="navImage menuHome" src="/images/navmenu/home_resized.png" alt="black and white home icon" /></Link>
-                <Link onClick={() => handleClick("1")} className={parseInt(sessionStorage.getItem('currentPage') ?? '1') === 1 ? "navlink inline active" : "navlink inline"} to="/solar_system"><img className="navImage solarsystemHome" src="/images/navmenu/distance_resized.png" alt="black and white distance calculator icon" /></Link>
-                <Link onClick={() => handleClick("2")} className={parseInt(sessionStorage.getItem('currentPage') ?? '2') === 2 ? "navlink inline active" : "navlink inline"} to="/orbit"><img className="navImage orbitHome" src="/images/navmenu/orbit_resized.png" alt="black and white orbit icon" /></Link>
-                <Link onClick={() => handleClick("3")} className={parseInt(sessionStorage.getItem('currentPage') ?? '3') === 3 ? "navlink inline active" : "navlink inline"} to="/gallery"><img className="navImage galleryHome" src="/images/navmenu/gallery_resized.png" alt="black and white gallery icon" /></Link>
+                <Link onClick={() => handleClick("0")} className= "navlink inline" to="/"><img className="navImage menuHome" src={homeImage} alt="black and white home icon" /></Link>
+                <Link onClick={() => handleClick("1")} className={parseInt(sessionStorage.getItem('currentPage') ?? '1') === 1 ? "navlink inline active" : "navlink inline"} to="/solar_system"><img className="navImage solarsystemHome" src={distanceImage} alt="black and white distance calculator icon" /></Link>
+                <Link onClick={() => handleClick("2")} className={parseInt(sessionStorage.getItem('currentPage') ?? '2') === 2 ? "navlink inline active" : "navlink inline"} to="/orbit"><img className="navImage orbitHome" src={orbitImage} alt="black and white orbit icon" /></Link>
+                <Link onClick={() => handleClick("3")} className={parseInt(sessionStorage.getItem('currentPage') ?? '3') === 3 ? "navlink inline active" : "navlink inline"} to="/gallery"><img className="navImage galleryHome" src={galleryImage} alt="black and white gallery icon" /></Link>
             </div>
         </>
     );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,12 +5,19 @@ import './index.css';
 
 
 const Home = () => {
-  return (
-    <>
-      <div className="indexTitle">The Solar System</div>
-      <ImageAccordion />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+          <div className="indexTitle">The Solar System</div>
+          <div id = "introContainer">
+            <section id = "introduction">
+              <article className = "introPanel">
+                Welcome to The Solar System, a site that will give you a brief tour of the Sun and planets.<br/><br/>- Calculate distance between planets<br/>- View a proportnially correct simulation of planet orbits.<br/>- Discover an image gallery of space sights.
+              </article>	
+            </section>
+          </div>
+          <ImageAccordion />
+          <Footer />
+        </>
+    );
 }
 export default Home;
